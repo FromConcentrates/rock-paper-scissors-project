58846590f051b0205f10c348d51951c3806ab4ef
Christen <257850355+FromConcentrates@users.noreply.github.com>
2026-02-19 17:49:12 -0500

    Create initial getComputerChoice function
    The function is a proof of concept and functional. I will expand on it
    further in upcoming commits.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello World")
+
+/* Create a function named getComputerChoice
+getComputerChoice will randomly return one of the following
+values: "rock," "paper," or "scissors"
+*/
+
+
+
+
+function getComputerChoice(play) {
+    play = Math.random()
+    if (play < .5) console.log(('rock'))
+        else alert('no')
+}
+
+
+getComputerChoice();
