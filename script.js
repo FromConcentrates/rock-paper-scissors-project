d9cc70af97a43100a9af4c3ded19644005603685
Christen <257850355+FromConcentrates@users.noreply.github.com>
2026-02-20 12:00:03 -0500

    Create working getComputerChoice()
    Ran into an issue with writing the full expressions within the function, as laid out
    in my pseudocode. The chained comparisons were evaluating incorrectly because they were being read as arithmetic expressions,
    and being evaluated as 'true' or 'false.'
    
    Furthermore, I removed the redundant lower bound conditions after realizing that the code runs from the top down,
    so the first available 'true' would run, even if the subsequent conditions were true.

--- script.js
+++ script.js
@@ -8,17 +8,22 @@ Function is named getComputerChoice
 Parameter of the function is "turn"
 Turn is equal to Math.random(), which returns a random number between 0 and 1.
 If turn is greater than or equal to zero but less than one third, return rock
-If turn is greater than or equal to one third, but less than two thirds, return paper
-If turn is greater than or equal to two thirds, but less than or equal to 1, return scissors
-Else, return "error"
+Else If turn is greater than or equal to one third, but less than two thirds, return paper
+Else If If turn is greater than or equal to two thirds, but less than or equal to 1, return scissors
 */
 
-
 function getComputerChoice(turn) {
-    turn = Math.random()
-    if (turn < .5) console.log(('rock'))
-        else alert('no')
+    turn = Math.random();
+    if (turn < (1/3)) { // removed redundant condition (turn >= 0) since Math.random() never returns a negative number
+        console.log('rock');
+    } else if (turn < (2/3)) {
+        console.log('paper');
+// Else if with conditions isn't necessary here because it's the only other option
+    } else {
+        console.log('scissors'); 
+    }
 }
+
 
 
 getComputerChoice();
