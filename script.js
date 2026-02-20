d3ce2f1b4f4521d2a03dba7813eee3a2849aad6f
Christen <257850355+FromConcentrates@users.noreply.github.com>
2026-02-20 16:08:45 -0500

    Complete playGame()
    Repeated playRound() with different variable assignments, five times, in order to assign
    5 rounds to playGame. playGame generates a message based on game win counts via console.log,
    as the function's return value is not captured elsewhere.

--- script.js
+++ script.js
@@ -47,6 +47,31 @@ Initialize those variables with the value of 0. */
 let humanScore = 0;
 let computerScore = 0;
 
+/* Step 5: Write the logic to play a single round
+Your game will be played round by round. You will write a function that takes
+the human and computer player choices as arguments, plays a single round, increments the round 
+ winner’s score and logs a winner announcement.
+
+Create a new function named playRound.
+Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to
+take the human and computer choices as arguments.
+Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”,
+or other variations.
+Write the code for your playRound function to console.log a string value representing the round winner,
+Such as: “You lose! Paper beats Rock”.
+Increment the humanScore or computerScore variable based on the round winner. 
+
+Pseudocode: 
+The game is played in rounds. 
+The human and the computer each play a choice as arguments. This is a single round.
+The round winner's score (variable) is incremented
+A winner's accouncement is logged to the console
+
+If the computer wins the round, the computer's score is incremented by one
+Else if the human wins the round, the human's score is incremented by one
+Else the round is a tie and neither score is incremented by one.
+*/
+
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
     if (humanChoice ==='paper' && computerChoice ==='rock') {
@@ -78,35 +103,59 @@ function playRound(humanChoice, computerChoice) {
     }
     
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+/*
+Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, 
+keeps track of the scores and declares a winner at the end.
 
-playRound(humanSelection, computerSelection);
+Create a new function named playGame.
+Move your playRound function and score variables so that they’re declared inside of the new playGame function
+Play 5 rounds by calling playRound 5 times.
+Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. 
+Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to 
+recall the choice functions to get new choices for each round.
+Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change 
+the return values to something more useful.
+If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
 
-
-/* Step 5: Write the logic to play a single round
-Your game will be played round by round. You will write a function that takes
-the human and computer player choices as arguments, plays a single round, increments the round 
- winner’s score and logs a winner announcement.
-
-Create a new function named playRound.
-Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to
-take the human and computer choices as arguments.
-Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”,
-or other variations.
-Write the code for your playRound function to console.log a string value representing the round winner,
-Such as: “You lose! Paper beats Rock”.
-Increment the humanScore or computerScore variable based on the round winner. 
-
-Pseudocode: 
-The game is played in rounds. 
-The human and the computer each play a choice as arguments. This is a single round.
-The round winner's score (variable) is incremented
-A winner's accouncement is logged to the console
-
-If the computer wins the round, the computer's score is incremented by one
-Else if the human wins the round, the human's score is incremented by one
-Else the round is a tie and neither score is incremented by one.
+Create a new function named playGame
+Place playRound and score variables inside playGame
+Play 5 rounds by calling playRound 5 times
+Recall the choice functions to get new choices for each round
+Compare computer and human scores
+If computer score is greater than human score, declare computer the winner
+If human score is greater than computer score, declare human the winner
 */
 
+function playGame() {
 
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    round1 = playRound(humanSelection, computerSelection);
+
+    const humanSelection2 = getHumanChoice();
+    const computerSelection2 = getComputerChoice();
+    round2 = playRound(humanSelection2, computerSelection2);
+
+    const humanSelection3 = getHumanChoice();
+    const computerSelection3 = getComputerChoice();
+    round3 = playRound(humanSelection3, computerSelection3);
+
+    const humanSelection4 = getHumanChoice();
+    const computerSelection4 = getComputerChoice();
+    round4 = playRound(humanSelection4, computerSelection4);
+
+    const humanSelection5 = getHumanChoice();
+    const computerSelection5 = getComputerChoice();
+    round5 = playRound(humanSelection5, computerSelection5);
+
+    if (humanScore > computerScore) {
+        console.log('The human wins!');
+    }
+    else if (computerScore > humanScore) {
+        console.log('The computer wins!');
+    }
+    else {
+        console.log('It is a tie')}
+}
+
+playGame();
