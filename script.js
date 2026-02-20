cdd91f504739647b632897d3f2c0f6106f77c1ce
Christen <257850355+FromConcentrates@users.noreply.github.com>
2026-02-20 13:11:13 -0500

    Complete getHumanChoice();
    The function will return a choice of rock, paper, or scissors via a prompt.

--- script.js
+++ script.js
@@ -41,11 +41,11 @@ The title is a greeting to the user, and the default will include 'rock,' 'paper
 */
 
 function getHumanChoice() {
-    choice = prompt("Do you choose Rock, Paper, or Sciossors?", 'Ex: Rock');
-    alert(`You chose ${choice}!`);
+    choice = prompt("Do you choose Rock, Paper, or Sciossors?", 'Ex: Scissors');
+    return choice;
 
 }
-getHumanChoice();
+console.log(getHumanChoice());
 
 
 
